59f024248ada913637cad6d160980d5c0675bf8e
Aryaman M Singha <91653939+Aryamnsls@users.noreply.github.com>
2023-06-17 03:43:34 +0530

    GITHUB

---
+++ GalleryApp/index.js
@@ -0,0 +1,57 @@
+// /**
+//  * @format
+//  */
+
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import App from './App.js';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
+
+
+import React from 'react';
+import { StyleSheet, View, FlatList, Image, Dimensions } from 'react-native';
+
+const images = [
+  { id: 1, source: require('./images/native1.png') },
+  { id: 2, source: require('./images/native2.jpg') },
+  { id: 3, source: require('./images/native3.png') },
+  // Add more images here
+];
+
+const { width } = Dimensions.get('window');
+const imageWidth = width / 3;
+
+const GalleryApp = () => {
+  const renderItem = ({ item }) => (
+    <Image source={item.source} style={styles.image} resizeMode="cover" />
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={images}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+        numColumns={3}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+    paddingHorizontal: 10,
+    backgroundColor: '#fff',
+  },
+  image: {
+    width: imageWidth,
+    height: imageWidth,
+    margin: 5,
+  },
+});
+
+export default GalleryApp;
